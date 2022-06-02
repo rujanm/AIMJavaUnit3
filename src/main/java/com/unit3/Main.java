a2d17c931dfbc6441f4c8e93593a03b18c1af3ec
rujanm <rujan.m@gmail.com>
2022-06-02 11:29:07 -0500

    commit

--- src/main/java/com/unit3/Main.java
+++ src/main/java/com/unit3/Main.java
@@ -1,8 +1,44 @@
 package com.unit3;
 
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
 public class Main {
+
+static void main() {
+    
+    Rectangle rectangle1 = new Rectangle(4,40);
+    Rectangle rectangle2 = new Rectangle(3.5,5);
+    System.out.println("This program creates two rectangle objects and displays their width, height," +
+    "\n" +  "area and perimeter." + "\n");
+    System.out.println("Rectangle 1:");
+    System.out.println("heigth = " + String.format("%.2f", rectangle1.getHeight()));
+    System.out.println("width = " + String.format("%.2f", rectangle1.getWidth()));
+    System.out.println("area = " + String.format("%.2f", rectangle1.getArea()));
+    System.out.println("perimeter = " + String.format("%.2f", rectangle1.getPerimeter()));
+    System.out.println("\n");
+
+
+    System.out.println("Rectangle 2:");
+    System.out.println("heigth = " + String.format("%.2f", rectangle2.getHeight()));
+    System.out.println("width = " + String.format("%.2f", rectangle2.getWidth()));
+    System.out.println("area = " + String.format("%.2f", rectangle2.getArea()));
+    System.out.println("perimeter = " + String.format("%.2f", rectangle2.getPerimeter()));
+    System.out.println("\n");
+
+    System.out.println("Goodbye...");
+}
+
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+        main();
+ 
     }
 }
--- src/main/java/com/unit3/Rectangle.java
+++ src/main/java/com/unit3/Rectangle.java
@@ -1,10 +1,42 @@
 package com.unit3;
 
-/**
- * Hello world!
- *
- */
-public class Rectangle 
-{
-    
+public class Rectangle {
+    double width = 1;
+double height = 1;
+
+public Rectangle(){
+
+}
+
+public Rectangle(double height, double width){
+    this.height = height;
+    this.width = width;
+}
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+
+    public double getArea(){
+        double area = this.height * this.width;
+        return area;
+    }
+
+    public double getPerimeter(){
+        double perimeter = 2*(this.height + this.width);
+        return perimeter;
+    }
 }
